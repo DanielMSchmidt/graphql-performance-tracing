401e321d00be22ee50a1b93ba1063ae56f8696c6
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-06-12 22:56:47 +0200

    implement network requests for data

--- graphql-base/schema.js
+++ graphql-base/schema.js
@@ -1,4 +1,5 @@
 const { makeExecutableSchema } = require("graphql-tools");
+const fetch = require("node-fetch");
 
 const typeDefs = `
 type User {
@@ -15,12 +16,67 @@ type Query {
 `;
 
 const USERS = [
-  { id: 1, name: "Peter", friends: [2] },
-  { id: 2, name: "Klaus", friends: [] }
+  {
+    id: 1,
+    name: "Dave",
+    friends: [2],
+    mentor: null,
+    skills: "Go, Ruby"
+  },
+  {
+    id: 2,
+    name: "Peter",
+    friends: [],
+    mentor: null,
+    skills: "Javascript"
+  },
+  {
+    id: 3,
+    name: "Norman",
+    friends: [1, 2],
+    mentor: 4,
+    skills: "Javascript"
+  },
+  {
+    id: 4,
+    name: "Raphael",
+    friends: [],
+    mentor: 1,
+    skills: "Ruby, Go, DevOps"
+  }
 ];
 
+function findUser(id) {
+  return USERS.find(user => user.id === id);
+}
+
+function getUser(id) {
+  return fetch(`${USER_ENDPOINT}?timeout=${SPREAD_DELAY}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(findUser(id))
+  }).then(res => res.json());
+}
+
+const SPREAD_DELAY = 50;
+const USERS_ENDPOINT = process.env.USERS_ENDPOINT;
+const USER_ENDPOINT = process.env.USER_ENDPOINT;
+
 function getUsers() {
-  return Promise.resolve(USERS);
+  return fetch(`${USERS_ENDPOINT}?timeout=${SPREAD_DELAY}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(
+      USERS.map(user => ({
+        endpoint: USER_ENDPOINT,
+        body: JSON.stringify(user)
+      }))
+    )
+  }).then(res => res.json());
 }
 
 const schema = makeExecutableSchema({
@@ -33,9 +89,7 @@ const schema = makeExecutableSchema({
     },
     User: {
       friends(obj, _args, _context) {
-        return obj.friends.map(friendId =>
-          USERS.find(user => user.id === friendId)
-        );
+        return Promise.all(obj.friends.map(friendId => getUser(friendId)));
       }
     }
   }
