3199aa05bd126bf3758c810a17f2eb761142bc0a
Daniel Schmidt <dschmidt@mesosphere.com>
2018-10-27 15:05:12 -0700

    automatic setup

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 This is the base for a blogpost / talk about how to measure performance traces.
 It contains different directories representing different stages of how to do performance profiles.
+
+## Deployment
+
+1. Get a DC/OS cluster
+2. Install the DC/OS CLI and configure it with the give cluster
+3. Add the ssh-key for the cluster to your SSH agent
+4. run `./deploy-dcos.sh` this opens all the pages you need (public agent + master)
--- get-public-ip.sh
+++ get-public-ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for id in $(dcos node --json | jq --raw-output '.[] | select(.reserved_resources.slave_public != null) | .id'); do 
-    dcos node ssh --option StrictHostKeyChecking=no --option LogLevel=quiet --master-proxy --mesos-id=$id "curl -s ifconfig.co";
-done 2>/dev/null
+HOST=$(dcos node --json | jq --raw-output '.[] | select(.reserved_resources.slave_public != null) | .id')
+
+dcos node ssh --option StrictHostKeyChecking=no --option LogLevel=quiet --master-proxy --mesos-id="$HOST" "curl -s ifconfig.co";
